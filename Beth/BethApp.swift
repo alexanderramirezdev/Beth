9cd874ec0c134224eb75abed0fcbbeeea70f8b31
Alexander Ramirez <314813792+alexanderramirezdev@users.noreply.github.com>
2026-08-09 11:03:58 -0600

    Initial Commit

---
+++ Beth/BethApp.swift
@@ -0,0 +1,17 @@
+//
+//  BethApp.swift
+//  Beth
+//
+//  Created by Alexander Ramirez on 8/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beth
+//
+//  Created by Alexander Ramirez on 8/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
